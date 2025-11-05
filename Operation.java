6541b4a2ecd990860c639ed06eac65ccbd6e64e2
Soulaima Tounsi <tounsisoulaima@gmail.com>
2025-11-05 04:36:07 -0800

    Ajout du test de la division

--- Operation.java
+++ Operation.java
@@ -5,4 +5,6 @@ public class Operation {
     }
     public static int soustraction(int x, int y) { return x - y; }
     public static int addition(int x, int y) { return x + y; }
+    public static double division(double x, double y) {return y != 0 ? x / y : 0;}
+
 }
