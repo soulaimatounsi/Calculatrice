cecf20b6de6d7b20519d373a2563d69fc47da745
Soulaima Tounsi <tounsisoulaima@gmail.com>
2025-11-05 04:41:13 -0800

    Ajout d’un message bienvenue

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 2;
         int b = 3;
+        System.out.println("Bonjour");
         System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
         System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
         System.out.println(a + " + " + b + " = " + Operation.addition(a, b));
