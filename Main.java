43e4061315006d545b27f205bc5f66fdd6a8535a
Soulaima Tounsi <tounsisoulaima@gmail.com>
2025-11-05 04:26:36 -0800

    Ajout de la soustraction et de l’addition et leurs tests

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
+        System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
+        System.out.println(a + " + " + b + " = " + Operation.addition(a, b));
+    }
+}
--- Operation.java
+++ Operation.java
@@ -1,6 +1,8 @@
 public class Operation {
-    System.out.println("Bonjour");
+
     public static int multiplication(int x, int y) {
         return x * y;
     }
+    public static int soustraction(int x, int y) { return x - y; }
+    public static int addition(int x, int y) { return x + y; }
 }
