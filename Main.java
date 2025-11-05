f019a2c486f11245fa9fcb4a12aca3cc5f1acb0a
Soulaima Tounsi <tounsisoulaima@gmail.com>
2025-11-05 04:49:15 -0800

    Rajout d’un test de la vérification de la valeur de y dans la division

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main {
         System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
         System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
         System.out.println(a + " + " + b + " = " + Operation.addition(a, b));
+         // ✅ Test de la division normale
         System.out.println(a + " / " + b + " = " + Operation.division(a, b));
+
+        // ⚠️ Test de la division par zéro
+        int c = 0;
+        System.out.println(a + " / " + c + " = " + Operation.division(a, c));
     }
 }
